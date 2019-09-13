41ce36c0b82897c43dca6edac7fbe9bbac684d65
Yassine El Adlouni <yeladlouni@gmail.com>
2019-09-13 15:08:01 +0100

    Add files via upload

--- lab6.sql
+++ lab6.sql
@@ -3,20 +3,22 @@ DECLARE
 BEGIN
     -- Création de la table temporaire copy_emp
     BEGIN
-        EXECUTE IMMEDIATE 'CREATE TABLE copy_emp AS SELECT * FROM employees';
+        EXECUTE IMMEDIATE 'CREATE TABLE hr.copy_emp AS SELECT * FROM employees';
         COMMIT;
     END;
-        
-    UPDATE employees
-    SET salary = 12000
-    WHERE employee_id = 110;
     
-    INSERT INTO employees 
+    BEGIN
+        UPDATE hr.employees
+        SET salary = 12000
+        WHERE employee_id = 110;
+    END;
+    
+    INSERT INTO hr.employees 
     VALUES(EMPLOYEES_SEQ.nextval, 'dummy', 'dummy', 'a@a.com', '002126000000',
-    '01-01-2019', 'IT_PROG', NULL, NULL, 10);
+    '01-01-2019', 'IT_PROG', 10000, NULL, NULL, 10);
     
-    MERGE INTO copy_emp c
-        USING employees e
+    MERGE INTO hr.copy_emp c
+        USING hr.employees e
         ON (e.employee_id = c.employee_id)
     WHEN MATCHED THEN
         UPDATE SET
@@ -26,7 +28,7 @@ BEGIN
     WHEN NOT MATCHED THEN
         INSERT VALUES(e.employee_id, e.first_name, e.last_name, e.email,
         e.phone_number, e.hire_date, e.job_id, e.salary,
-        e.commission_pct, manager_id, e.department_id);
+        e.commission_pct, e.manager_id, e.department_id);
         
             
 END;
